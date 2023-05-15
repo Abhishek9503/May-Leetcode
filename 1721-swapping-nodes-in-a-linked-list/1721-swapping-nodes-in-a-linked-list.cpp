87f3ee58d973570245c734ac21faebcfa2a3a7df
Abhishek Yadav <103676667+Abhishek9503@users.noreply.github.com>
2023-05-15 23:53:57 +0530

    Time: 735 ms (9.67%), Space: 180.1 MB (54.58%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -8,45 +8,77 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// Brute force
+
+
+// class Solution {
+// public:
+//     int findLength(ListNode* head)
+//     {
+//         int l=0;
+//         while(head!=NULL)
+//         {
+//             head=head->next;
+//             l++;
+//         }
+//         return l;
+//     }
+    
+//     ListNode* swapNodes(ListNode* head, int k) {
+//         // kth from start
+//         // k th node from End =(Length-K+1)th node from start
+        
+//         int L=findLength(head);
+        
+//         int size =k;
+        
+//         ListNode * Node1=head;
+//         while(size>1)
+//         {
+//             Node1=Node1->next;
+//             size--;
+//         }
+        
+//         int size2 = L -k+1;
+        
+//         ListNode * Node2=head;
+//             while(size2>1)
+//         {
+//             Node2=Node2->next;
+//             size2--;
+//         }
+        
+//         swap(Node1->val,Node2->val);
+        
+//         return head;
+//     }
+// };
+
+
+//  Second approach   yt code story with mik
 class Solution {
 public:
-    int findLength(ListNode* head)
-    {
-        int l=0;
-        while(head!=NULL)
-        {
-            head=head->next;
-            l++;
-        }
-        return l;
-    }
-    
     ListNode* swapNodes(ListNode* head, int k) {
-        // kth from start
-        // k th node from End =(Length-K+1)th node from start
+        ListNode*p1=NULL;
+        ListNode* p2=NULL;
+        ListNode* temp=head;
         
-        int L=findLength(head);
-        
-        int size =k;
-        
-        ListNode * Node1=head;
-        while(size>1)
+        while(temp!=NULL)
         {
-            Node1=Node1->next;
-            size--;
+            if(p2!=NULL)
+            {
+                p2=p2->next;
+            }
+            k--;
+            
+            if(k==0)
+            {
+                p1=temp;
+                p2=head;  // activate kr denge
+            }
+            temp=temp->next;
         }
-        
-        int size2 = L -k+1;
-        
-        ListNode * Node2=head;
-            while(size2>1)
-        {
-            Node2=Node2->next;
-            size2--;
-        }
-        
-        swap(Node1->val,Node2->val);
-        
-        return head;
+        swap(p1->val,p2->val);
+            return head;
     }
 };
