b39cc8a38f3bbcf2d40056422ef684d668c95949
Abhishek Yadav <103676667+Abhishek9503@users.noreply.github.com>
2023-05-26 23:39:32 +0530

    Time: 29 ms (50.80%), Space: 8.2 MB (97.34%) - LeetHub

---
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    
+    int n;
+    int t[2][101][101]; //t[2][n+1][n+1] - 2*(n+1)*(n+1) ~n^2 * n ~ O(n^3)
+    int solveForAlice(vector<int>& piles, int person, int i, int M) {
+        
+        if(i >= n)
+            return 0;
+        
+        if(t[person][i][M] != -1)
+            return t[person][i][M];
+        
+        int result = (person == 1) ? -1 : INT_MAX;
+        
+        int stones = 0;
+        
+        for(int x = 1; x <= min(2*M, n-i); x++) {
+            
+            stones += piles[i+x-1];
+            
+            if(person == 1) { //Alice
+                result = max(result, stones + solveForAlice(piles, 0, i+x, max(M, x)));
+            } else { //Bob
+                result = min(result, solveForAlice(piles, 1, i+x, max(M, x)));
+            }
+            
+        }
+        
+        return t[person][i][M] = result;
+        
+    }
+    
+    int stoneGameII(vector<int>& piles) {
+        n = piles.size();
+        
+        memset(t, -1, sizeof(t));
+        
+        return solveForAlice(piles, 1, 0, 1);
+    }
+};
